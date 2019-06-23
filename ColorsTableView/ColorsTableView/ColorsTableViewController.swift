663e6a2e2ce7ec1d6d413f91810132183305a047
mrbrianjiang1121 <byjiang@outlook.com>
2019-06-23 11:21:39 -0500

    Created a Table View for this challenge

---
+++ ColorsTableView/ColorsTableView/ColorsTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ColorsTableViewController.swift
+//  ColorsTableView
+//
+//  Created by Brian Jiang on 6/23/19.
+//  Copyright © 2019 Brian Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ColorsTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
+        
+        cell.textLabel?.text = colors[indexPath.row]
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
